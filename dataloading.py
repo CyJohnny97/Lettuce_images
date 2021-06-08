d217ccd099fb72e8216c6bd352620c7c618c95d0
awkirby <awkirby09@gmail.com>
2021-06-08 18:56:24 +0300

    Added dataloading.py file

---
+++ dataloading.py
@@ -0,0 +1,7 @@
+"""
+Conversion of images (RGB and Depth) into a dataset.
+Enables train-test-validation pipelines to be developed for model training.
+Transformations informed by data exploration notebook.
+"""
+
+
